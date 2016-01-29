9fc6a6088fb50816a9bf57c7b0e56c7dcaf0de56
borjanG <borjan.geshkovski@etu.u-bordeaux.fr>
2016-01-29 21:53:36 +0100

    Indent

--- data/aspirateur.py
+++ data/aspirateur.py
@@ -7,20 +7,20 @@ import data.monde
 class Aspirateur(object):
 	""" Aspirateur constructor """
 
-	def __init__(self):
-	  self.__stock = 150    #a varier plus tard
-	  self.__run = True     
-	  self.__batterie = 100 #a varier plus tard
-		# pass
+  def __init__(self):
+	self.__stock = 150    #a varier plus tard
+	self.__run = True     
+	self.__batterie = 100 #a varier plus tard
+	# pass
 
-	@property 
-	def vider(self):
-		self.__stock = 0
+  @property 
+  def vider(self):
+	self.__stock = 0
 		
-	@property 
-	def arret(self):
-		self.__run = False
+  @property 
+  def arret(self):
+	self.__run = False
 
-	# #def aspirer(self, t, pos@):
-	# #def gauche(self, t, pos@):
-	# #def droite(self, t, pos@):
+  # def aspirer(self, t, pos@):
+  # def gauche(self, t, pos@):
+  # def droite(self, t, pos@):
