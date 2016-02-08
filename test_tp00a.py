d5e0d2e612cf5141c784dc95a0ddcf26c1ba1697
borjanG <borjan.geshkovski@etu.u-bordeaux.fr>
2016-02-08 22:13:26 +0100

    Borjan: je suis trop tired, flemme de coder, 34/34 sur les nouveaux test tout va nickel on gere trop

--- test_tp00a.py
+++ test_tp00a.py
@@ -3,7 +3,7 @@
 
 __author__ = "mmc <marc-michel dot corsini at u-bordeaux dot fr>"
 __usage__ = "tests unitaires pour la première réalisation aspi autonome"
-__date__ = "03.02.16"
+__date__ = "04.02.16"
 __version__ = "0.1"
 
 ## remplacer XXX par le nom de votre fichier
@@ -12,6 +12,7 @@ import data.monde as tp00
 
 # NE RIEN MODIFIER A PARTIR D'ICI
 import random
+import copy
 # un test est de la forme test_xxx() où xxx est la méthode testée
 # un test n'a pas de paramètre
 # un sous-test est de la forme subtest_xxx_yyy( params ) il est normalement
@@ -33,17 +34,48 @@ def check_property(p,msg='default',letter='E'):
         
     return _
 
+def subtest_readonly(obj,lattr):
+    """ vérification de chaque attribut de obj en lecture seule """
+    _s = ''
+    for att in lattr:
+        oldv = copy.deepcopy(getattr(obj,att))
+        try:
+            _s += '.'
+            setattr(obj,att,42)
+            if oldv != getattr(obj,att): _s += 'E'
+        except Exception:
+            _s += '.'
+        if _s[-2:] != '..' : return '%s: avant %s apres %s' % (att,oldv,getattr(obj,att))
+        
+    return _s
+    
+            
 def main():
     _msg = ''
     _s = ''
+    _toDO = []
+    try:
+        aspi = tp00.Aspirateur()
+        world = tp00.Monde(aspi) 
+    except Exception:
+        return "test_tp00 is required to succeed"
+        
     # Controle des attributs/méthodes requis
     _attr = "capteurs actions vivant".split()
+    _toDO.extend( _attr )
     _meth = "setReward getEvaluation getDecision".split()
+    _toDO.extend( _meth )
+    
     for _ in _attr:
         _msg += check_property(hasattr(tp00.Aspirateur,_),'%s inconnu' % _)
     print("attributs Aspirateur:",_msg)
+    _s += _msg 
+    
+    _msg = subtest_readonly(aspi,_attr)
+    print("attributs are ReadOnly",_msg)
     _s += _msg ; _msg = ''
         
+    
     for _ in _meth:
         _msg += check_property(hasattr(tp00.Aspirateur,_),'%s inconnu' % _)
         if _msg[-1] == '.' :
@@ -52,12 +84,19 @@ def main():
     _s += _msg ; _msg = ''
         
     _attr = "agent perfGlobale historique".split()
+    _toDO.extend( _attr )
     _meth = "updateWorld applyChoix getPerception step simulation".split()
+    _toDO.extend( _meth )
+    
     for _ in _attr:
         _msg += check_property(hasattr(tp00.Monde,_),'%s inconnu' % _)
     print("attributs Monde:",_msg)
     _s += _msg ; _msg = ''
         
+    _msg = subtest_readonly(world,_attr)
+    print("attributs are ReadOnly",_msg)
+    _s += _msg ; _msg = ''
+    
     for _ in _meth:
         _msg += check_property(hasattr(tp00.Monde,_),'%s inconnu' % _)
         if _msg[-1] == '.' :
@@ -65,6 +104,18 @@ def main():
     print("méthodes Monde:",_msg)
     _s += _msg ; _msg = ''
 
+    
+    # if '.'*len(_s) != _s :
+    #     print(_s)
+    #     print('correction needed, something missing')
+    # else:
+    #     # On passe aux tests plus complexes
+    #     for meth in _toDO :
+    #         meth = 'test_'+meth
+    #         _tmp = eval(meth)()
+    #         print(meth,_tmp)
+    #         _s += _tmp
+    # 
         
     # Bilan
     _all = len(_s) 
@@ -73,4 +124,4 @@ def main():
 
 if __name__ == '__main__' :
     print("succ %d fail %d sum %d, rate = %.2f" % main())
-    print("expected >> succ {0} fail 0 sum {0} rate = 100.00".format(22))
+    print("expected >> succ {0} fail 0 sum {0} rate = 100.00".format(34))
